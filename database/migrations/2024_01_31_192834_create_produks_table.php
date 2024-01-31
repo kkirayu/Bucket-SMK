bb392b3d0c6c8c5c73070163ab36cd02b5185a0a
kkirayu <kukicha76@gmail.com>
2024-01-31 19:38:41 +0700

    add_produk

---
+++ database/migrations/2024_01_31_192834_create_produks_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('produks', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama');
+            $table->string('kategori');
+            $table->text('descripsi');
+            $table->text('inovasi');
+            $table->string('sekolah');
+            $table->string('photo');
+            $table->string('video_produk');
+            $table->text('nama_tim');
+            $table->text('jurusan');
+            $table->text('material');
+            $table->integer('harga');
+            $table->date('tahun_produksi');
+            $table->string('merk_dagang');
+            $table->text('sertifikasi_haki');
+            $table->text('sertifikasi_halal');
+            $table->text('sni');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('produks');
+    }
+};
